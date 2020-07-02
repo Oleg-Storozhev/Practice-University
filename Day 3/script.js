0ec5f81a2e5b1362e5a8ec27627c5e154eda5071
Oleg Storozhev <54131483+Oleg-Storozhev@users.noreply.github.com>
2020-07-02 22:00:32 +0300

    Update script.js

--- Day 3/script.js
+++ Day 3/script.js
@@ -1,118 +1,83 @@
-function F(x, y) {
-  return x * x + y * y - 9;
-}
-// =====================================================================
-
-var pixctx = mypixcanvas.getContext("2d");
-let alph = Math.PI / 6;
-
-class Graphics1d {
+class Graphics2d {
   constructor() {
-    this.xmin = -5.0;
-    this.xmax = 5.0;
+    this.xmin = -10.0;
+    this.xmax = 10.0;
     this.ymin = -10.0;
     this.ymax = 10.0;
-    this.W = 500;
-    this.H = 500;
-    mypixcanvas.width = this.W;
-    mypixcanvas.height = this.H;
-    this.imgData = pixctx.getImageData(0, 0, this.W, this.H);
-    this.z = new Float64Array(this.W * this.H);
+    this.W = 450;
+    this.H = 450;
+    this.z = new Float64Array(this.W * this.H * 4);
+    this.f = function f(x, y) {
+      return x*x+y*y-81;
+    };
   }
-
   evaluate() {
     let i = 0;
-    let X = 0; //Screen X
-    let Y = 0; //Screen Y
-    for (let p = 0; p < this.H * this.W * 4; p += 4) {
-      let x = X / 10;
-      let y = -Y / 10;
-      x -= this.W / 20;
-      y += this.H / 20;
-      const th = 1 / 250;
+    let Dx = (this.xmax - this.xmin) / this.W;
+    let Dy = (this.ymax - this.ymin) / this.H;
+    for (let x = this.xmin; x <= this.xmax; x += Dx) {
+      for (let y = this.ymin; y <= this.ymax; y += Dy) {
+        this.z[i] = this.f(x, y);
+        i++;
+      }
+    }
+  }
+  
+  draw() {
+    var canvas = document.getElementById("canvas");
+    var pixctx = canvas.getContext("2d");
+    var imgData = pixctx.getImageData(0, 0, this.W, this.H);
+    var RGBA = imgData.data;
+
+    canvas.height = this.H;
+    canvas.width = this.W;
+
+    let alph = Math.PI / 6;
+    let X = 0;
+    let Y = 0;
+    let dx = this.W / (this.xmax - this.xmin);
+    let dy = this.H / (this.ymax - this.ymin);
+
+    for (let p = 0; p < this.W * this.H * 4; p += 4) {
+      let x = X / this.xmax;
+      let y = -Y / this.ymax;
+      x -= dx;
+      y += dy;
+      const th = 10 / Math.min(this.W, this.H);
       let axis_x = Math.abs(y) < th;
       let axis_y = Math.abs(x) < th;
-      let axes = (axis_x + axis_y) * 100;
+      let axes = (axis_x + axis_y) * 64;
       let grid_vertical = Math.abs(x % 1) < th;
       let grid_horizontal = Math.abs(y % 1) < th;
-      let grid = (grid_vertical + grid_horizontal) * 75;
+      let grid = (grid_vertical + grid_horizontal) * 64;
       let axes_and_grid = axes + grid;
-      this.z[i] = F(x, y);
-      let a = Math.abs(this.z[i]);
+      
+      let F = this.f(x, y);
+      let a = Math.abs(F);
       let F1 = (255 / a) * 6;
 
-      var RGBA = this.imgData.data;
-
-      if (this.z[i] > 0) {
-        RGBA[p + 0] = F1;
-      }
+      if (F > 0) RGBA[p + 0] = F1;
       RGBA[p + 1] = axes_and_grid;
-
-      if (this.z[i] < 0) {
-        RGBA[p + 2] = F1;
-      }
+      if (F < 0) RGBA[p + 2] = F1;
       RGBA[p + 3] = 255;
-
       X++;
       if (X == this.W) {
         X = 0;
         Y++;
       }
-      i++;
     }
+    pixctx.putImageData(imgData, 0, 0);
+    pixctx.fillStyle = "white";
+    pixctx.fillText("xmin=", 10, this.H - 40);
+    pixctx.fillText("ymin=", 10, this.H - 20);
+    pixctx.fillText("xmax=", this.W - 100, 20);
+    pixctx.fillText("ymax=", this.W - 100, 40);
+    pixctx.fillText(this.xmin, 70, this.H - 40);
+    pixctx.fillText(this.ymin, 70, this.H - 20);
+    pixctx.fillText(this.xmax, this.W - 30, 20);
+    pixctx.fillText(this.ymax, this.W - 30, 40);
+    pixctx.stroke();
   }
-
-  draw() {
-    let i = 0;
-    let Lx = this.W;
-    let lx = this.xmax - this.xmin;
-    let Ly = this.H;
-    let ly = this.ymax - this.ymin;
-    let dx = lx / this.W;
-    let Sx = Lx / lx;
-    let Sy = Ly / ly;
-    let X = (0 - this.xmin) * Sx + 0;
-    let Y = -(0 - this.ymin) * Sy + this.H;
-    for (
-      let p = 0;
-      p < this.H * this.W * (this.xmax - this.xmin / this.W);
-      p += this.xmax - this.xmin / this.W
-    ) {
-      let x = X;
-      let y = -Y;
-      const th = 1 / 250;
-      let axis_x = Math.abs(y) < th;
-      let axis_y = Math.abs(x) < th;
-      let axes = (axis_x + axis_y) * 100;
-      let grid_vertical = Math.abs(x % 1) < th;
-      let grid_horizontal = Math.abs(y % 1) < th;
-      let grid = (grid_vertical + grid_horizontal) * 75;
-      let axes_and_grid = axes + grid;
-      let a = Math.abs(this.z[i]);
-      let F1 = (255 / a) * 6;
-
-      var RGBA = this.imgData.data;
-
-      if (this.z[i] > 0) {
-        RGBA[p + 0] = F1;
-      }
-      RGBA[p + 1] = axes_and_grid;
-
-      if (this.z[i] < 0) {
-        RGBA[p + 2] = F1;
-      }
-      RGBA[p + 3] = 255;
-
-      X++;
-      if (X == this.xmax) {
-        X = 0;
-        Y++;
-      }
-      i++;
-    }
-    pixctx.putImageData(this.imgData, 0, 0);
-  }
-
   autodraw() {
     let min = 999999;
     let max = -999999;
@@ -130,8 +95,7 @@ class Graphics1d {
   }
 }
 
-let g = new Graphics1d();
-
+let g = new Graphics2d();
 g.evaluate();
-g.autodraw();
+//g.autodraw();
 g.draw();
